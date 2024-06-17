d94783bf9624de74f64db23121a8dbaf8500fcbc
David Carcelen <davidcarcelen@MacBook-Pro-de-David-2.local>
2024-06-17 13:36:39 +0200

    properties

--- Tasca2/Nivel1/S04T02N01CarcelenDavid/src/main/java/cat/itacademy/barcelonactiva/carcelen/david/s04/t02/n01/controllers/FruitController.java
+++ Tasca2/Nivel1/S04T02N01CarcelenDavid/src/main/java/cat/itacademy/barcelonactiva/carcelen/david/s04/t02/n01/controllers/FruitController.java
@@ -1,21 +1,19 @@
 package cat.itacademy.barcelonactiva.carcelen.david.s04.t02.n01.controllers;
 
-import cat.itacademy.barcelonactiva.carcelen.david.s04.t02.n01.model.Fruit;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import cat.itacademy.barcelonactiva.carcelen.david.s04.t02.n01.model.domain.Fruit;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FruitController {
-    @GetMapping("/fruit/add")
+    @PostMapping("/fruit/add")
     private String addFruit (Fruit fruit){
         return "You added " + fruit.getName();
     }
-    @GetMapping("/fruit/update")
+    @PutMapping("/fruit/update")
     private String updateFruit (Fruit fruit){
         return "You updated " + fruit.getName();
     }
-    @GetMapping("/fruit/delete/{id}")
+    @DeleteMapping("/fruit/delete/{id}")
     private String deleteFruit (@PathVariable(required = true)Fruit fruit){
         int id = fruit.getId();
         return "You deleted " + fruit.getName();
@@ -31,17 +29,4 @@ public class FruitController {
         return "You have it";
     }
 
-
-
-    //La classe ubicada al paquet controllers (FruitaController, per exemple), haurà de ser capaç de donar resposta a les següents peticions per actualitzar i consultar informació:
-    //
-    //http://localhost:8080/fruita/add
-    //
-    //http://localhost:8080/fruita/update
-    //
-    //http://localhost:8080/fruita/delete/{id}
-    //
-    //http://localhost:8080/fruita/getOne/{id}
-    //
-    //http://localhost:8080/fruita/getAll
 }
--- Tasca2/Nivel1/S04T02N01CarcelenDavid/src/main/java/cat/itacademy/barcelonactiva/carcelen/david/s04/t02/n01/model/domain/Fruit.java
+++ Tasca2/Nivel1/S04T02N01CarcelenDavid/src/main/java/cat/itacademy/barcelonactiva/carcelen/david/s04/t02/n01/model/domain/Fruit.java
@@ -1,4 +1,4 @@
-package cat.itacademy.barcelonactiva.carcelen.david.s04.t02.n01.model;
+package cat.itacademy.barcelonactiva.carcelen.david.s04.t02.n01.model.domain;
 
 import jakarta.persistence.Entity;
 
