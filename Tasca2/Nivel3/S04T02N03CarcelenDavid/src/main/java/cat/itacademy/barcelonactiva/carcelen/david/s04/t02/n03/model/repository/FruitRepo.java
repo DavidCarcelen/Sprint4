2afd9ba43a9a275440f525502fa0f52600166e13
David Carcelen <davidcarcelen@MacBook-Pro-de-David-2.local>
2024-06-20 23:14:26 +0200

    ready

--- Tasca2/Nivel3/S04T02N03CarcelenDavid/src/main/java/cat/itacademy/barcelonactiva/carcelen/david/s04/t02/n03/model/repository/FruitRepo.java
+++ Tasca2/Nivel3/S04T02N03CarcelenDavid/src/main/java/cat/itacademy/barcelonactiva/carcelen/david/s04/t02/n03/model/repository/FruitRepo.java
@@ -1,11 +1,11 @@
 package cat.itacademy.barcelonactiva.carcelen.david.s04.t02.n03.model.repository;
 
 import cat.itacademy.barcelonactiva.carcelen.david.s04.t02.n03.model.domain.Fruit;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface FruitRepo extends JpaRepository<Fruit,String> {
+public interface FruitRepo extends MongoRepository<Fruit,String> {
 
 }
